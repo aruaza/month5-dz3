8a542edf7880be7bfc7f4ed3a4925f144356df72
Aruaza <aruaaraoa@gmail.com>
2024-04-27 19:37:33 +0300

    dz3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const baseUrl = 'http://fakestoreapi.com/products';
+    const [data, setData] = useState();
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        const getAsync = async () => {
+            setLoading(true);
+            setError(null);
+            try {
+                const response = await axios.get(baseUrl);
+                setData(response.data);
+            } catch (e) {
+                setError(true);
+            } finally {
+                setLoading(false);
+            }
+        };
+        getAsync();
+    }, []);
+
+    if (loading) {
+        return <div>LOADING...</div>;
+    }
+    if (error) {
+        return <div>ERROR my brother</div>;
+    }
+    if (!data || data.length === 0) {
+        return <div>нету данных</div>;
+    }
+
+    return (
+        <div style={{display:'flex', justifyContent:'space-around', flexWrap:'wrap', gap:'20px'}}>
+            {data.map((item) => (
+                <div style={{width:'250px', border:'2px solid black'}} key={item.id}>
+                    <span>{item.title}</span>
+                    <img width='200px' src={item.image} alt=""/>
+                </div>
+            ))}
+        </div>
+    );
+};
 
 export default App;
